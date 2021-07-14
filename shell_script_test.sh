ae425aec822564ea9c17b47f0249c8521a2bc016
sannuprajapati <78226381+sannuprajapati@users.noreply.github.com>
2021-07-14 14:38:06 +0530

    Update shell_script_test.sh
    commit

--- shell_script_test.sh
+++ shell_script_test.sh
@@ -1,20 +1,33 @@
+echo "enter input file name:\c"
+read fin
+echo "enter output file  name:\c"
+read fout
 echo "enter a file name:\c"
-read fname
-if [ -z "$fname"]
+read R_word
+echo "enter a file name:\c"
+read N_word
+
+
+if [ -z "$fin"]
 then
       exit
 fi
 
 terminal='tty'
 
-exec < $fname
+exec < $fin
 
 count=1
 while read line
 do 
   echo $count.$line
   count='expr $count + 1'
+  sed  's /$R_word/$N_word /g' $fin
 
 done
 
-exec
+
+
+
+
+
